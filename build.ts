9c02988a08e7152a9e567052c6ae9a94441e475c
sakkke <84666033+sakkke@users.noreply.github.com>
2024-04-08 21:35:40 +0900

    chore: init extension

---
+++ build.ts
@@ -0,0 +1,22 @@
+import { $ } from 'bun';
+import { cp } from 'node:fs/promises';
+
+const describe = await $`git describe --tags`.text();
+
+const version = describe.length !== 0 ? describe : '0.1.0-snapshot';
+console.log(`version: ${version}`);
+
+const resourcesPath = './resources';
+const buildPath = './build'
+const extensionName = `moodle_data-${version}`;
+const extensionPath = `${buildPath}/${extensionName}`;
+const zipPath = `${extensionPath}.tar.gz`;
+
+await cp(resourcesPath, extensionPath, { recursive: true });
+
+await Bun.build({
+  entrypoints: ['./index.ts'],
+  outdir: extensionPath,
+});
+
+await $`tar -v -c -f ${zipPath} -zC ${buildPath} ${extensionName}`;
